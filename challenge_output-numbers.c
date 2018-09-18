dca77db7b5fb01b6d30841a93eb4152bba40f3f6
sebasexy <spider_cbaz_10@hotmail.com>
2018-09-18 16:15:09 -0500

    Challenge Output Numbers

---
+++ challenge_output-numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int j = -10;
+
+    for(i = 1; i <= 20; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    while(j <= 10)
+    {
+        printf("%d\n", j++);
+    }
+
+    return 0;
+}
+#include <stdio.h>
+
